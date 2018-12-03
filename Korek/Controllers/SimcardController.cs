4bee0b7bd6ef9ef9d7529cf7e8def6e57311b77e
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-03 04:35:39 +0300

    Add all CRUD functionality for SIM Cards lists with jQuery and Ajax requests

--- Korek/Controllers/SimcardController.cs
+++ Korek/Controllers/SimcardController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Korek.Models;
 
 namespace Korek.Controllers
 {
@@ -12,6 +14,53 @@ namespace Korek.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0, int simId = 0)
+        {
+            ViewBag.Id = id;
+            if (simId == 0)
+                return View(new Simcard());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Simcards.Where(x => x.SimcardID == simId).Where(x => x.CorporateID == id).FirstOrDefault<Simcard>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Simcard simcard)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (simcard.SimcardID == 0)
+                {
+                    db.Simcards.Add(simcard);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Save Successfuly" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(simcard).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Update Successfuly" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Simcard simcard = db.Simcards.Where(x => x.CorporateID == id).FirstOrDefault<Simcard>();
+                db.Simcards.Remove(simcard);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
