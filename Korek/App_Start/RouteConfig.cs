12e553d6c4dcd0e706a7555e230e4bd3b95bc61f
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-03 04:08:33 +0300

    Add GetData method and corporate index

--- Korek/App_Start/RouteConfig.cs
+++ Korek/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Korek
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Corporate", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Korek/Controllers/CorporateController.cs
+++ Korek/Controllers/CorporateController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Korek.Models;
 
 namespace Korek.Controllers
 {
@@ -12,6 +13,29 @@ namespace Korek.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Corporate> empList = db.Corporates.ToList<Corporate>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Corporate());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Corporates.Where(x => x.CorporateID == id).FirstOrDefault<Corporate>());
+                }
+            }
         }
     }
 }
